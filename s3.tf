f77b17f6ce9c5944bd7b8080127d9f544a58afd2
Vaishnavi <vyshukammari@gmail.com>
2025-08-24 11:07:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,15 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-example-bucket"
+# Random ID for uniqueness
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
+# S3 bucket resource
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "swiggy-bucket-${random_id.suffix.hex}"   # unique name
+}
+
+# Enable versioning (using new resource, not deprecated)
 resource "aws_s3_bucket_versioning" "example_versioning" {
   bucket = aws_s3_bucket.example_bucket.id
 
@@ -10,3 +17,4 @@ resource "aws_s3_bucket_versioning" "example_versioning" {
     status = "Enabled"
   }
 }
+
